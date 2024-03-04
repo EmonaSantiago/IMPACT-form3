05f7a51d472b13482c96d42635d31b91861d9896
Emona Santiago <emonasantiago1207@gmail.com>
2024-03-04 21:56:16 +0100

    Footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,65 @@
+<footer class="footer">
+    <div class="container">
+      <h2>FOR MORE QUESTIONS...</h2>
+      <div class="links">
+        <a href="/">+31 6 22428876</a>
+        <a href="/">info.impactofficials@gmail.com</a>
+        <a href="/">impact.officials</a>
+        <a href="/">impact.officials</a>
+      </div>
+    </div>
+  </footer>
+
+  <style>
+ .footer {
+  color: #fff;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  border-top: 1px solid #fff; /* Add a thin white line */
+}
+
+.container {
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h2 {
+  margin: 0;
+}
+
+.links {
+  display: flex;
+  /* flex-wrap: wrap; */
+}
+
+.links a {
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px; /* Spacing between links */
+}
+
+/* Media query for mobile */
+@media only screen and (max-width: 600px) {
+  .container {
+    flex-direction: column;
+    text-align: center;
+    padding: 20px;
+  }
+
+  h2 {
+    margin-bottom: 1em;
+  }
+
+  .links {
+    flex-direction: column;
+  }
+
+  .links a {
+    margin: 5px 0;
+  }
+}
+
+  </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
     import "../styles/styles.css";
+    import Footer from "$lib/components/footer.svelte";
 </script>
 
 
   <main id="main-content">
     <slot />
   </main>
+  <Footer></Footer>
