11631973e20395b78d329949729f9e56e034f84c
Emona Santiago <emonasantiago1207@gmail.com>
2024-03-05 10:24:11 +0100

    model page + responsive

---
+++ src/lib/components/2form.svelte
@@ -0,0 +1,322 @@
+<body>
+    <div class="container">
+       <form action="https://formspree.io/f/mgegkpdq" method="POST">
+   
+   
+
+ <!-- Full name -->
+  <div class="row">
+    <div class="col-25">
+      <label for="full-name">Full Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" name="full-name" id="full-name" placeholder="First and Last" required="">
+    </div>
+  </div>
+
+  
+    <!-- Link insta web -->
+    <div class="row">
+        <div class="col-25">
+          <label for="link">Link</label>
+        </div>
+        <div class="col-75">
+          <input type="text" name="link" id="link" placeholder="link insta or website" required="">
+        </div>
+      </div>
+
+        <!-- Gender -->
+          <div class="row">
+            <div class="col-25">
+              <label for="gender">Please select your gender identity:</label>
+            </div>
+            <div class="col-75">
+              <select id="gender" name="gender">
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Non-binary">Non-binary</option>
+                <option value="Prefer not to say">Prefer not to say</option>
+              </select>
+            </div>
+          </div>  
+
+      <!-- Length -->
+      <div class="row">
+        <div class="col-25">
+          <label for="Lenght">Length</label>
+        </div>
+        <div class="col-75">
+          <input type="text" name="length" id="length" placeholder="Your length..." required="">
+        </div>
+      </div>
+      
+
+      <!-- Size top -->
+      <div class="row">
+        <div class="col-25">
+          <label for="size-top">size-top</label>
+        </div>
+        <div class="col-75">
+          <input type="text" name="size-top" id="size-top" placeholder="Your size-top..." required="">
+        </div>
+      </div>
+      
+  <!-- Size bottom -->
+  <div class="row">
+    <div class="col-25">
+      <label for="size-bottom">size-bottom</label>
+    </div>
+    <div class="col-75">
+      <input type="text" name="size-bottom" id="size-bottom" placeholder="Your Size-bottom..." required="">
+    </div>
+  </div>
+  
+   <!-- Shoe size -->
+   <div class="row">
+    <div class="col-25">
+      <label for="shoe-size">shoe-size</label>
+    </div>
+    <div class="col-75">
+      <input type="text" name="shoe-size" id="shoe-size" placeholder="Your shoe-size..." required="">
+    </div>
+  </div>
+
+     <!-- Email -->
+     <div class="row">
+        <div class="col-25">
+          <label for="email-address">Email Address</label>
+        </div>
+        <div class="col-75">
+          <input type="text" name="email" id="email-address" placeholder="email@domain.tld" required="">
+        </div>
+      </div>
+
+
+      <!-- Telefoon nummer -->
+      <div class="row">
+        <div class="col-25">
+          <label for="phone-nummer">Phone number</label>
+        </div>
+        <div class="col-75">
+          <input type="text" name="phone-name" id="phone-number" placeholder="+31()" required="">
+        </div>
+      </div>
+
+          <!-- Motivatie message -->
+          <div class="row">
+            <div class="col-25">
+              <label for="motivation-message">Motivation message</label>
+            </div>
+            <div class="col-75">
+              <textarea rows="3" name="message" id="message" required="" placeholder="Motivation message "></textarea>
+          <input type="hidden" name="_subject" id="email-subject" value="Department Contact Form Submission">
+            </div>
+          </div>
+    
+               <!-- How did you hear about this form? -->
+      <div class="row">
+        <div class="col-25">
+          <label for="reclame">How did you hear about this form?</label>
+        </div>
+        <div class="col-75">
+          <select id="reclame" name="reclame">
+            <option value="Instagram">Instagram</option>
+            <option value="Tiktok">Tiktok</option>
+            <option value="Sticker">Sticker</option>
+            <option value="mutuals">Mutuals</option>
+          </select>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <input type="submit" value="SUBMIT">
+      </div>
+    </form>
+  </div>
+
+</body>
+
+
+
+<style>
+    * {
+    margin: 0;
+    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+    
+    :root {
+    --main-yellow-: #F8F053;
+    }
+    
+    h1 {
+    font-size: 110px;
+    color: #F8F053;
+    text-align: center;
+    }
+    
+    body {
+    background-image: url(/src/darkimg.png);
+    margin: 1em;
+    /* height: 1000px; */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    }
+    
+    /* button */
+    .button-container {
+    display: flex;
+    justify-content: space-between; /* Distribute buttons evenly */
+    flex-wrap: wrap; /* Wrap buttons to next line if necessary */
+    margin: 0 auto;
+    max-width: 600px; /* Adjust max-width as needed */
+    }
+    
+    .button {
+    flex: 1; /* Each button takes equal space */
+    max-width: 30%; /* Maximum width for each button */
+    text-align: center;
+    margin: 5px; /* Adjust margin as needed */
+    padding: 10px;
+    /* background-color: #F8F053; */
+    color: white;
+    text-decoration: none;
+    border: none;
+    border: solid #F8F053 1px;
+    /* border-radius: 5px; */
+    transition: background-color 0.3s;
+    }
+    
+    .button:hover {
+    background-color: #F8F053;
+    }
+    
+    @media (max-width: 600px) {
+    /* Adjust for smaller screens */
+    .button {
+    max-width: 100%; /* Full width on smaller screens */
+    }
+    }
+    
+    .button1 {
+    flex: 1; /* Each button takes equal space */
+    max-width: 30%; /* Maximum width for each button */
+    text-align: center;
+    margin: 5px; /* Adjust margin as needed */
+    padding: 10px;
+    background-color: #F8F053;
+    color: black;
+    text-decoration: none;
+    border: none;
+    /* border-radius: 5px; */
+    transition: background-color 0.3s;
+    }
+    
+    .button1:hover {
+    background-color: #F8F053;
+    }
+    
+    @media (max-width: 600px) {
+    /* Adjust for smaller screens */
+    .button1 {
+    max-width: 100%; /* Full width on smaller screens */
+    }
+    }
+    
+    
+    /* form */
+    * {
+    box-sizing: border-box;
+    }
+    
+    input[type=text], select, textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    resize: vertical;
+    border-radius: 5px;
+    }
+    label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+    color: white;
+    float: inline-start;
+    }
+    
+    input[type=submit] {
+    background-color: #F8F053;
+    color: black;
+    padding: 12px 20px;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    width: 100%;
+    }
+ 
+    input[type="file" i] {
+     color: white;
+     /* padding: 12px 12px 12px 0; */
+    }
+    
+    /* input[type=submit]:hover {
+    background-color: #45a049;
+    } */
+    
+    .container {
+    border-radius: 5px;
+    padding: 10px;
+    }
+    
+    .col-25 {
+    float: left;
+    width: 25%;
+    margin-top: 6px;
+    }
+    
+    /* .col-75 {
+    float: left;
+    width: 75%;
+    margin-top: 6px;
+    } */
+    
+    /* Clear floats after the columns */
+    .row:after {
+    content: "";
+    display: table;
+    clear: both;
+    }
+    
+    #file-button {
+      color: greyc;
+    }
+    
+    /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+    @media screen and (min-width: 600px){
+    .col-25, .col-75 {
+    width: 100%;
+    margin-top: 0;
+    }
+    
+    input[type=submit] {
+      margin-top: 1em;
+      width: 100%;
+    }
+    }
+ 
+    @media screen and (max-width: 600px){ 
+     .col-25, .col-75 {
+    width: 100%;
+    margin-top: 0;
+    }
+    
+    input[type=submit] {
+      margin-top: 1em;
+      width: 100%;
+    }
+    }
+ 
+    
+    
+    </style>
---
+++ src/lib/components/2navigation.svelte
@@ -0,0 +1,73 @@
+<div class="button-container">
+    <a href="/creative" class="button">CREATIVE</a>
+    <a href="/" class="button1">MODEL</a>
+    <a href="/crew" class="button">CREW</a>
+  </div>
+  
+  <style>
+    :root {
+        --impact-yellow: #F8F053;
+    }
+
+
+    .button-container {
+      display: flex;
+      justify-content: space-between; /* Distribute buttons evenly */
+      flex-wrap: wrap; /* Wrap buttons to next line if necessary */
+      margin: 0 auto;
+      max-width: 600px; /* Adjust max-width as needed */
+    }
+  
+    .button {
+      flex: 1; /* Each button takes equal space */
+      max-width: 30%; /* Maximum width for each button */
+      text-align: center;
+      margin: 5px; /* Adjust margin as needed */
+      margin-top: 1em;
+      padding: 10px;
+      /* background-color: #F8F053; */
+      color: white;
+      text-decoration: none;
+      border: none;
+      border: solid white 1px;
+      transition: background-color 0.3s;
+    }
+  
+    .button:hover {
+      background-color: #F8F053;
+      color: black;
+    }
+
+    .button1 {
+      flex: 1; /* Each button takes equal space */
+      max-width: 30%; /* Maximum width for each button */
+      text-align: center;
+      margin: 5px; /* Adjust margin as needed */
+      margin-top: 1em;
+      padding: 10px;
+      background-color: #F8F053;
+      color: black;
+      text-decoration: none;
+      border: none;
+      border: solid white 1px;
+      border-radius: 5px;
+      transition: background-color 0.3s;
+    }
+  
+    .button1:hover {
+      color: #F8F053;
+      background: none;
+    }
+  
+    @media (max-width: 600px) {
+      /* Adjust for smaller screens */
+      .button {
+        max-width: 100%; /* Full width on smaller screens */
+      }
+
+      .button1 {
+        max-width: 100%; /* Full width on smaller screens */
+      }
+    }
+  </style>
+  
